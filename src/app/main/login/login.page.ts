f5995ec7c7119a54e204fda09d88b1ecc5e6beaa
BrianMartelA <brianmartelabdala@gmail.com>
2024-11-15 21:50:38 -0300

    Formulario

--- src/app/main/login/login.page.ts
+++ src/app/main/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
+  form = new FormGroup({
+    email: new FormControl('',[Validators.required,Validators.email]),
+    password: new FormControl('',[Validators.required])
+  })
   constructor() { }
 
   ngOnInit() {
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,15 +4,16 @@ import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormComponent } from './form/form.component';
 
 
 
 @NgModule({
-  declarations: [FooterComponent,HeaderComponent],
+  declarations: [FooterComponent,HeaderComponent,FormComponent],
   imports: [
     CommonModule,
     IonicModule,ReactiveFormsModule,FormsModule
   ],
-  exports: [FooterComponent,HeaderComponent]
+  exports: [FooterComponent,HeaderComponent,FormComponent,ReactiveFormsModule]
 })
 export class SharedModule { }
